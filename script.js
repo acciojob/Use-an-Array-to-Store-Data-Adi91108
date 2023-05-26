6626925f34dc0882847f2284d70a97b3291ce1e3
Aditya sen <130759840+Adi91108@users.noreply.github.com>
2023-05-26 20:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+let adiArr=[
+	// [
+	// 	{
+	'one',2,'three',true,"hi"
+	// },
+		// {'two',3,'four',false,"hello"},
+		// {'three',4,'five',true,"bye"},
+		// {'four',5,'six',false,"thanks"},
+		// {'five',,'seven',true,"welcome"}
+	// ]
+];
+console.log(adiArr.length);
